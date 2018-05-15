d5c2bfa4d45e08ae5e130393b42d42f64f8dcdc5
MegatronChen <kongfufatcat@163.com>
2018-05-15 17:17:11 +0800

    2018.05.15t1716-Mac Air-Chapter 17 finished

--- BuildInModule_Learn/GUI/2018.05.14Strat_GUI.py
+++ BuildInModule_Learn/GUI/2018.05.14Strat_GUI.py
@@ -61,3 +61,28 @@ print('\n')
 
 # 2018.05.15
 # Lenovo received
+# Mac learn
+# Example7.18
+# from tkinter import *
+#
+# class HelloButton(Button):
+#     def __init__(self,parent=None,**config):
+#         Button.__init__(self,parent,**config)
+#         self.pack()
+#         self.config(command=self.callback)
+#
+#     def callback(self):
+#         print('Goodbye world...')
+#         self.quit()
+#
+# # if __name__=='__main__':
+# #     HelloButton(text='Hello subclass world').mainloop()
+#
+#
+# # Example7.19
+# class MyButton(HelloButton):
+#     def callback(self):
+#         print('Ignoring press!...')
+#
+# if __name__=='__main__':
+#     MyButton(None,text='Hello subclass world').mainloop()
