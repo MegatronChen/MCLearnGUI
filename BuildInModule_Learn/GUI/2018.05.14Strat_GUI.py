feb96c7b2ee59167436e319cd87a08baba383694
MegatronChen <kongfufatcat@163.com>
2018-05-15 19:43:36 +0800

    2018.05.15t1945-Mac Air-before supper

--- BuildInModule_Learn/GUI/2018.05.14Strat_GUI.py
+++ BuildInModule_Learn/GUI/2018.05.14Strat_GUI.py
@@ -86,3 +86,58 @@ print('\n')
 #
 # if __name__=='__main__':
 #     MyButton(None,text='Hello subclass world').mainloop()
+
+
+
+# Chapter8
+# # Example8.1
+# from tkinter import *
+# root = Tk()
+# lablefont = ('times',20,'bold')
+# widget = Label(root,text='Hello config world')
+# widget.config(bg='black',fg='yellow')
+# widget.config(font=lablefont)
+# widget.config(height=3,width=20)
+# widget.pack(expand=YES,fill=BOTH)
+# root.mainloop()
+
+
+# # Example8.3
+# import sys
+# from tkinter import Toplevel,Button,Label
+#
+# win1 = Toplevel()
+# win2 = Toplevel()
+#
+# Button(win1,text='Spam',command=sys.exit).pack()
+# Button(win2,text='SPAM',command=sys.exit).pack()
+#
+# Label(text='Popups').pack()
+# win1.mainloop()
+# import sys
+# help(sys.exit)
+
+
+# # Example8.5
+# from tkinter import *
+# root = Tk()
+#
+# trees = [('The Larch!','light blue'),
+#          ('The Pine!','light green'),
+#          ('The Giant Redwood!','red')]
+#
+# for (tree,color) in trees:
+#     win = Toplevel(root)
+#     win.title('Sing...')
+#     win.protocol('WM_DELETE_WINDOW',lambda:None)
+#     win.iconbitmap('py-blue-trans-out.ico')
+#
+#     msg = Button(win,text=tree,command=win.destroy)
+#     msg.pack(expand=YES,fill=BOTH)
+#     msg.config(padx=10,pady=10,bd=10,relief=RAISED)
+#     msg.config(bg='black',fg=color,font=('times',30,'bold italic'))
+#
+# root.title('Lumberjack demo')
+# Label(root,text='Main window',width=30).pack()
+# Button(root,text='QUIT ALL',command=root.quit).pack()
+# root.mainloop()
