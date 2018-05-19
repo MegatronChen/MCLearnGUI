4e7c4a92c64f05efafe35e93992755c2bc0a649a
MegatronChen <kongfufatcat@163.com>
2018-05-19 14:15:00 +0800

    2018.05.19t1415-Mac Air-

--- BuildInModule_Learn/GUI/2018.05.14Strat_GUI.py
+++ BuildInModule_Learn/GUI/2018.05.14Strat_GUI.py
@@ -576,3 +576,24 @@ parts = []
 def addComponents(root):
     for demo in demoModules:
         module = __import__(demo)
+        part = module.Demo(root)
+        part.config(bd=2,relief=GROOVE)
+        part.pack(side=LEFT,expand=YES,fill=BOTH)
+        parts.append(part)
+
+def dumpState():
+    for part in parts:
+        print(part.__module__ + ':',end=' ')
+        if hasattr(part,'report'):
+            part.report()
+        else:
+            print('none')
+
+root = Tk()
+root.title('Frame')
+Label(root,text='Multiple Frame demo',bg='white').pack()
+Button(root,text='States',command=dumpState).pack(fill=X)
+Quitter(root).pack(fill=X)
+addComponents(root)
+root.mainloop()
+
