4243d3aeda4674c76d2ab8c705f9bae9764c63bb
MegatronChen <kongfufatcat@163.com>
2018-05-24 19:31:08 +0800

    2018.05.24t1900-Mac Air-Chapter9-Text

--- BuildInModule_Learn/GUI/2018.05.14Strat_GUI.py
+++ BuildInModule_Learn/GUI/2018.05.14Strat_GUI.py
@@ -1070,45 +1070,105 @@ print('\n')
 
 
 # Example9.10
+# from tkinter import *
+#
+# class ScrolledText(Frame):
+#     def __init__(self,parent=None,text='',file=None):
+#         Frame.__init__(self,parent)
+#         self.pack(expand=YES,fill=BOTH)
+#         self.makewidgets()
+#         self.settext(text,file)
+#
+#     def makewidgets(self):
+#         sbar = Scrollbar(self)
+#         text = Text(self,relief=SUNKEN)
+#         sbar.config(command=text.yview)
+#         text.config(yscrollcommand=sbar.set)
+#         sbar.pack(side=RIGHT,fill=Y)
+#         text.pack(side=LEFT,expand=YES,fill=BOTH)
+#         self.text = text
+#
+#     def settext(self,text='',file=None):
+#         if file:
+#             text = open(file,'r').read()
+#         self.text.delete('1.0',END)
+#         self.text.insert('1.0',text)
+#         self.text.mark_set(INSERT,'1.0')
+#         self.text.tag_add(SEL,1.0,3.5)
+#         self.text.focus()
+#         self.text.tag_remove(SEL,'1.0',2.0)
+#
+#     def gettext(self):
+#         return self.text.get('1.0',END+'-1c')
+#
+# if __name__ == '__main__':
+#     root = Tk()
+#     if len(sys.argv) > 1:
+#         st = ScrolledText(file=sys.argv[1])
+#     else:
+#         st = ScrolledText(text='Words\ngo here'*50)
+#
+#     def show(event):
+#         print(repr(st.gettext()))
+#     root.bind('<Key-Escape>',show)
+#     root.mainloop()
+
+
+# Example9.11
 from tkinter import *
+from tkinter.simpledialog import askstring
+from tkinter.filedialog import asksaveasfilename
+from quitter import Quitter
+from scrolledtext import ScrolledText
 
-class ScrolledText(Frame):
-    def __init__(self,parent=None,text='',file=None):
-        Frame.__init__(self,parent)
-        self.pack(expand=YES,fill=BOTH)
-        self.makewidgets()
-        self.settext(text,file)
+class SimpleEditor(ScrolledText):
+    def __init__(self,parent=None,file=None):
+        frm = Frame(parent)
+        frm.pack()
+        Button(frm, text='Save',command=self.onSave).pack(side=LEFT)
+        Button(frm, text='Cut', command=self.onCut).pack(side=LEFT)
+        Button(frm, text='Paste', command=self.onPaste).pack(side=LEFT)
+        Button(frm, text='Find', command=self.onFind).pack(side=LEFT)
+        Quitter(frm).pack(side=LEFT)
+        ScrolledText.__init__(self,parent,file=file)
+        self.text.config(font=('courier',9,'normal'))
 
-    def makewidgets(self):
-        sbar = Scrollbar(self)
-        text = Text(self,relief=SUNKEN)
-        sbar.config(command=text.yview)
-        text.config(yscrollcommand=sbar.set)
-        sbar.pack(side=RIGHT,fill=Y)
-        text.pack(side=LEFT,expand=YES,fill=BOTH)
-        self.text = text
+    def onSave(self):
+        filename = asksaveasfilename()
+        if filename:
+            alltext = self.gettext()
+            open(filename,'w').write(alltext)
 
-    def settext(self,text='',file=None):
-        if file:
-            text = open(file,'r').read()
-        self.text.delete('1.0',END)
-        self.text.insert('1.0',text)
-        self.text.mark_set(INSERT,'1.0')
-        self.text.tag_add(SEL,1.0,3.5)
-        self.text.focus()
-        self.text.tag_remove(SEL,'1.0',2.0)
+    def onCut(self):
+        text = self.text.get(SEL_FIRST,SEL_LAST)
+        self.text.delete(SEL_FIRST,SEL_LAST)
+        self.clipboard_clear
+        self.clipboard_append(text)
 
-    def gettext(self):
-        return self.text.get('1.0',END+'-1c')
+    def onPaste(self):
+        try:
+            text = self.selection_get(selection='CLIPBOARD')
+            self.text.insert(INSERT,text)
+        except TclError:
+            pass
+
+    def onFind(self):
+        target = askstring('SimpleEditor','Search String?')
+        if target:
+            where = self.text.search(target,INSERT,END)
+            if where:
+                print(where)
+                pastit = where + ('+%dc' % len(target))
+                self.text.tag_remove(SEL,'1.0',END)
+                self.text.tag_add(SEL,where,pastit)
+                self.text.mark_set(INSERT,pastit)
+                self.text.see(INSERT)
+                self.text.focus()
 
 if __name__ == '__main__':
-    root = Tk()
     if len(sys.argv) > 1:
-        st = ScrolledText(file=sys.argv[1])
+        SimpleEditor(file=sys.argv[1]).mainloop()
     else:
-        st = ScrolledText(text='Words\ngo here'*50)
+        SimpleEditor().mainloop()
 
-    def show(event):
-        print(repr(st.gettext()))
-    root.bind('<Key-Escape>',show)
-    root.mainloop()
+
