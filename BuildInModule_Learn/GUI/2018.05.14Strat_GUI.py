c442fc8ab3c8dc1a0468531ec4c3138804628b55
MegatronChen <kongfufatcat@163.com>
2018-05-24 11:50:51 +0800

    2018.05.24t1200-Mac Air-Chapter9-Text

--- BuildInModule_Learn/GUI/2018.05.14Strat_GUI.py
+++ BuildInModule_Learn/GUI/2018.05.14Strat_GUI.py
@@ -1016,3 +1016,99 @@ print('\n')
 #
 # if __name__ == '__main__':
 #     NewMenuDemo().mainloop()
+
+
+# 尝试保存文件框
+# from tkinter.filedialog import *
+# help(asksaveasfile)
+# # help(asksaveasfilename)
+# a1 = asksaveasfile()-不能用下面的with open打开
+# # a1 = asksaveasfilename()
+# # print(a)
+# # print(a1)
+# with open(a1,'w') as f:
+#     f.write('hello world!')
+
+
+# # Example9.9
+# from tkinter import *
+#
+# class ScrolledList(Frame):
+#     def __init__(self,options,parent=None):
+#         Frame.__init__(self,parent)
+#         self.pack(expand=YES,fill=BOTH)
+#         self.makeWidgets(options)
+#
+#     def handleList(self,event):
+#         index = self.listbox.curselection()
+#         label = self.listbox.get(index)
+#         self.runCommand(label)
+#
+#     def makeWidgets(self,options):
+#         sbar = Scrollbar(self)
+#         list = Listbox(self,relief=SUNKEN)
+#         sbar.config(command=list.yview)
+#         list.config(yscrollcommand=sbar.set)
+#         sbar.pack(side=RIGHT,fill=Y)
+#         list.pack(side=LEFT,expand=YES,fill=BOTH)
+#         pos = 0
+#         for label in options:
+#             list.insert(pos,label)
+#             pos += 1
+#         list.config(selectmode=SINGLE,setgrid=1)
+#         # list.config(selectmode=EXTENDED,setgrid=1)
+#         list.bind('<Double-1>',self.handleList)
+#         self.listbox = list
+#
+#     def runCommand(self,selection):
+#         print('You selected:',selection)
+
+# if __name__ == '__main__':
+#     options = (('Lumberjack-{0}'.format(x)) for x in range(20))
+#     ScrolledList(options).mainloop()
+#     # help(enumerate)
+
+
+# Example9.10
+from tkinter import *
+
+class ScrolledText(Frame):
+    def __init__(self,parent=None,text='',file=None):
+        Frame.__init__(self,parent)
+        self.pack(expand=YES,fill=BOTH)
+        self.makewidgets()
+        self.settext(text,file)
+
+    def makewidgets(self):
+        sbar = Scrollbar(self)
+        text = Text(self,relief=SUNKEN)
+        sbar.config(command=text.yview)
+        text.config(yscrollcommand=sbar.set)
+        sbar.pack(side=RIGHT,fill=Y)
+        text.pack(side=LEFT,expand=YES,fill=BOTH)
+        self.text = text
+
+    def settext(self,text='',file=None):
+        if file:
+            text = open(file,'r').read()
+        self.text.delete('1.0',END)
+        self.text.insert('1.0',text)
+        self.text.mark_set(INSERT,'1.0')
+        self.text.tag_add(SEL,1.0,3.5)
+        self.text.focus()
+        self.text.tag_remove(SEL,'1.0',2.0)
+
+    def gettext(self):
+        return self.text.get('1.0',END+'-1c')
+
+if __name__ == '__main__':
+    root = Tk()
+    if len(sys.argv) > 1:
+        st = ScrolledText(file=sys.argv[1])
+    else:
+        st = ScrolledText(text='Words\ngo here'*50)
+
+    def show(event):
+        print(repr(st.gettext()))
+    root.bind('<Key-Escape>',show)
+    root.mainloop()
